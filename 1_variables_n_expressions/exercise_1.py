5bf474362a15aed330729f588d7e6a2ed6693acd
Emmanuel <31184578+emthecoder@users.noreply.github.com>
2018-08-21 17:33:06 +0100

    Delete exercise_1.py

--- 1_variables_n_expressions/exercise_1.py
+++
@@ -1,3 +0,0 @@
-#place your code here
-wd0, wd1, wd2, wd3, wd4, wd5, wd6, wd7, wd8, wd9 ="All", "work", "and", "no", "play", "makes", "Jack", "a", "dull", "boy"
-print(wd0,wd1, wd2, wd3, wd4, wd5, wd6, wd7, wd8, wd9 +".\n")
